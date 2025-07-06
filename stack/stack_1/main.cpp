5e99c24f28940b8b3bf87898016c9c11ed2aaaf0
good <goodkosawanichakit@gmail.com>
2025-07-06 11:43:25 +0700

    Finish the first stack problems

---
+++ stack/stack_1/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+
+int is_valid(std::string &expression){
+  std::stack<char> arr;
+
+  for(char s: expression){
+    if(s == '('){
+      arr.push(s);
+    } 
+    else if(s == ')'){
+      if(arr.empty()){
+        return 1;
+      }
+      if(arr.top() == '('){
+        arr.pop();
+      } 
+      else{
+        return 1;
+      }
+    }
+  }
+  
+  if(!arr.empty()){
+    return 1;
+  }
+
+  return 0;
+}
+
+int main(){
+  std::string input;
+  std::getline(std::cin, input);
+  int result = is_valid(input);
+
+  if(result == 1){
+    std::cout << "error\n";
+  } else {
+    std::cout << "pass\n";
+  }
+
+  return 0;
+}
