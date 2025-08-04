b41067c27600343ac523109eea1d8bee2fe35ed9
goodkosawanichakit <goodkosawanichakit@gmail.com>
2025-08-04 09:50:27 +0700

    refactor current_process to pointer

--- CPUScheduling/LIFO/main.cpp
+++ CPUScheduling/LIFO/main.cpp
@@ -3,57 +3,63 @@
 #include <vector>
 #include <string>
 
-typedef struct process {
+struct Process {
   std::string PID;
   int A, T;
-} Process;
+};
 
 class ProcessScheduling {
   private:
-    std::vector<Process> processes;
-    std::stack<Process> processes_stack;
-    
+    std::vector<Process*> processes;
+    std::stack<Process*> processes_stack;
+
   public:
-    void arrive(Process new_process) {
+    void arrive(Process* new_process) {
       processes.push_back(new_process);
     }
 
     void run() {
       int current_time = 0;
       int index = 0;
-      bool has_current_process = false;
-      Process current_process;
-      do {
-        while(index < processes.size() && processes[index].A == current_time) {
+      Process* current_process = nullptr;
+
+      while (index < processes.size() || current_process || !processes_stack.empty()) {
+        while (index < processes.size() && processes[index]->A == current_time) {
           processes_stack.push(processes[index]);
           index++;
         }
-        if((!has_current_process || current_process.T == 0) && !processes_stack.empty()) {
+ 
+        if ((current_process || current_process->T == 0) && !processes_stack.empty()) {
           current_process = processes_stack.top();
           processes_stack.pop();
-          has_current_process = true;
         }
-        if(has_current_process && current_process.T > 0) {
-          std::cout << current_process.PID << ": " << current_process.T << '\n';
+
+        if (current_process && current_process->T > 0) {
+          std::cout << current_process->PID << ": " << current_process->T << '\n';
+          current_process->T--;
         }
-        current_process.T--;
-        current_time++; 
-      } while (index < processes.size() || has_current_process || !processes_stack.empty());
+        current_time++;
+      }
+    }
+
+    ~ProcessScheduling() {
+      for (auto p : processes) {
+        delete p;
+      }
     }
 };
 
 int main() {
-  int N, A, T;
-  std::string PID;
-  ProcessScheduling cpu;
+  int N;
   std::cin >> N;
+  ProcessScheduling cpu;
 
-  for(int i = 0; i < N; i++) {
-    Process P;
-    std::cin >> P.PID >> P.A >> P.T;
+  for (int i = 0; i < N; i++) {
+    auto* P = new Process;
+    std::cin >> P->PID >> P->A >> P->T;
     cpu.arrive(P);
   }
-  
+
   cpu.run();
   return 0;
 }
