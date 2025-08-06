e85b507dda6a3f9529145f33ce8d73c6f16f875a
goodkosawanichakit <goodkosawanichakit@gmail.com>
2025-08-06 14:51:02 +0700

    Finish LIFO

--- CPUScheduling/LIFO/main.cpp
+++ CPUScheduling/LIFO/main.cpp
@@ -3,6 +3,19 @@
 #include <vector>
 #include <string>
 
+/*
+vector <process*> proccesses = {
+  {P1, A = 2, T = 5}, {P2, A = 3, T = 2}, {P3, A = 2, T = 3}, {P4, A = 8, T = 2}}
+}
+__________________________________________________________________________________________________________
+| Time |  T1  |  T2  |  T3  |  T4  |  T5  |  T6  |  T7  |  T8  |  T9  |  T10 |  T11 |  T12 |  T13 |  T14 |  
+|  P1  |      |  XX  |  XX  |  XX  |  XX  |  XX  |      |      |      |      |      |      |      |      |
+|  P2  |      |      |      |      |      |      |      |      |      |      |      |      |  XX  |  XX  |
+|  P3  |      |      |      |      |      |      |  XX  |  XX  |  XX  |  XX  |      |      |      |      |  
+|  P4  |      |      |      |      |      |      |      |      |      |      |  XX  |  XX  |      |      |
+|______|______|______|______|______|______|______|______|______|______|______|______|______|______|______|
+*/
+
 struct Process {
   std::string PID;
   int A, T;
@@ -13,6 +26,19 @@ class ProcessScheduling {
     std::vector<Process*> processes;
     std::stack<Process*> processes_stack;
 
+    void loadprocess(int time) {
+      if(processes.empty()) return;
+      for (auto it = processes.begin(); it != processes.end(); ) {
+        Process* temp = *it;
+        if (temp->A == time) {
+          processes_stack.push(temp);  
+          it = processes.erase(it);
+        } else {
+          ++it;
+        }
+      }
+    }
+
   public:
     void arrive(Process* new_process) {
       processes.push_back(new_process);
@@ -20,30 +46,25 @@ class ProcessScheduling {
 
     void run() {
       int current_time = 0;
-      int index = 0;
-      Process* current_process = nullptr;
-
-      while (index < processes.size() || current_process || !processes_stack.empty()) {
-        while (index < processes.size() && processes[index]->A == current_time) {
-          processes_stack.push(processes[index]);
-          index++;
-        }
- 
-        if ((current_process || current_process->T == 0) && !processes_stack.empty()) {
+      Process* current_process = NULL;
+      do {
+        loadprocess(current_time);
+        if(!current_process && !processes_stack.empty()) {
           current_process = processes_stack.top();
           processes_stack.pop();
         }
-
-        if (current_process && current_process->T > 0) {
-          std::cout << current_process->PID << ": " << current_process->T << '\n';
+        if(current_process && current_process->T > 0) {
+          std::cout << current_process->PID << ": " << current_process->T << std::endl;
           current_process->T--;
+        } else if(current_process && current_process->T == 0) {
+          current_process = NULL;
         }
-        current_time++;
-      }
+        current_time++; 
+      } while(current_process || !processes_stack.empty());  
     }
 
     ~ProcessScheduling() {
-      for (auto p : processes) {
+      for (Process* p : processes) {
         delete p;
       }
     }
